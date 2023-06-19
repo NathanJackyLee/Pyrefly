b1fe1850a3ef1ea48830892ec98949adefcbe26a
asterynnn <134358662+asterynnn@users.noreply.github.com>
2023-06-19 12:44:14 +0700

    Update StageSelect.js

--- StageSelect.js
+++ StageSelect.js
@@ -8,58 +8,66 @@ function StageSelect(props){
 
     return(
 // {/* <center> */}
-    <div className='StageMainContainer'>
+<div className='StageMainContainer'>
         <div className='justify'>
             
             <div className="Darker">
                 <div className= {screening}></div>
             </div>
-            
 
-            <div style={{
+                <div className="RightRectangle2">
+
+                </div>
+
+                {/* <div style={{
                     position: 'fixed',
                     top: 0,
                     right: 0,
                     width: '390px', 
                     height: '767px',
                     backgroundColor: 'rgba(217, 217, 217, 1)' // Adjust the color and opacity as needed
-            }}></div>
+                }}></div> */}
 
-            <div className ='ChapterContainer'>
-                <div className ='Chapter'>
-                    <h1>{props.lang === 0 ? "Chapter" : "Episode"}</h1>
-                </div>
-            </div>    
+                    <div className ='ChapterContainer'>
+                        <div className ='Chapter'>
+                            <h1>{props.lang === 0 ? "Chapter" : "Episode"}</h1>
+                        </div>
+                    </div>    
 
-            <div className='ButtonContainer'>
-                <div className='Chapter1img'>   
-            </div>
-                <button className='buttonstage' onClick={() => props.setChapter(1)}>
-                    <strong>Chapter I</strong> The Calm Before the Storm
-                </button>
-            </div>
+                            <div className='ButtonContainer'>
+                                <div className='Chapter1img'>   
+                                </div>
+                                <button className='buttonstage' onClick={() => props.setChapter(1)}>
+                                    <strong>Chapter I</strong> The Calm Before the Storm
+                                </button>
 
-            <div className='ButtonContainer2'>
-            <div className='Chapter2img'>
-            </div>
-                <button className='buttonstage' onClick={() => props.setChapter(2)}>
-                   <strong>Chapter II</strong> The Chaos Begin
-                </button>
-            </div>
+                            </div>
 
-            <div className='ButtonContainer3'>
-                <div className='Chapter3img'>
-                </div>
-                <button className='buttonstage' onClick={() => props.setChapter(3)}>
-                    <strong>Chapter III</strong> A Call to Adventure
-                </button>
-            </div>
+                            <div className='ButtonContainer2'>
+                                <div className='Chapter2img'>
+                                </div>
+                                <button className='buttonstage' onClick={() => props.setChapter(2)}>
+                                    <strong>Chapter II</strong> The Chaos Begin
+                                </button>
+                            </div>
+
+                            <div className='ButtonContainer3'>
+                                <div className='Chapter3img'>
+                                </div>
+                                <button className='buttonstage' onClick={() => props.setChapter(3)}>
+                                    <strong>Chapter III</strong> A Call to Adventure
+                                </button>
+                            </div>
                         
-            <div className='BackButton'>
-                <button className='buttonstage2' onClick={() => props.setRoot(0)}>{props.lang === 0 ? "Back" : "Kembali"}</button>
-            </div>
+                        <div className='BackButton'>
+                            <button  onClick={() => props.setRoot(0)}>{props.lang === 0 ? "Back" : "Kembali"}</button>
+                        </div>
+                        
+                    
+                
+            
         </div>
-    </div>
+</div>
 
     )
 }
